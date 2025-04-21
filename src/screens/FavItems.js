40fc6131fcfb06cb753ccbcc3709ea83fad89430
firas hamila <firas.hm.1996@gmail.com>
2025-04-21 17:34:56 +0200

    aa

---
+++ src/screens/FavItems.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FavItems = () => {
+  return (
+    <View>
+      <Text>FavItems</Text>
+    </View>
+  );
+};
+
+export default FavItems;
+
+const styles = StyleSheet.create({});
